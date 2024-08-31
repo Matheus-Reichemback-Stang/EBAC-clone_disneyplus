83953bb329eee3802cc858ae9de3e0632a2ddc82
zkd <matheus.reichemback.stang@gmail.com>
2024-08-31 11:10:04 -0300

    fix: adicionado pasta dist, devido a erro de imagens

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,13 @@ const gulp = require('gulp');
 // Faz a compilação do SASS e Comprime o CSS de saída
 const sass = require('gulp-sass')(require('sass'));
 // Diminui o peso da imagens mantendo a qualidade
-const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 // Cria um Map
 const sourcemaps = require('gulp-sourcemaps');
 // Comprime o JavaScript
 const uglify = require('gulp-uglify');
 // Ofusca o JavaScript
 const obfuscate = require('gulp-obfuscate');
-// Minifica o HTML
-const htmlmin = require('gulp-htmlmin');
 
 // Minifica e Ofusca o JavaScript
 function scripts(){
@@ -28,12 +26,6 @@ function scripts(){
 //         .pipe(gulp.dest('./dist/images'))
 // }
 
-// Apenas envia as imagens para a pasta Dist - Distribution
-function images(){
-    return gulp.src('./src/images/**/*')
-        .pipe(gulp.dest('./dist/images'))
-}
-
 // Minifica o CSS de saída e faz um Map
 function styles(){
     return gulp.src('./src/styles/main.scss')
@@ -45,17 +37,8 @@ function styles(){
         .pipe(gulp.dest('./dist/styles'));
 }
 
-// Minifica o HTML
-function html(){
-    return gulp.src('./src/index.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('./dist'))
-}
-
-exports.default = gulp.parallel(html, images, styles, scripts);
+exports.default = gulp.series(styles, scripts);
 exports.watch = function(){
-    gulp.watch('./src/index.html', {ignoreInitial: false}, gulp.parallel(html))
-    gulp.watch('./src/images/**/*', {ignoreInitial: false}, gulp.parallel(images))
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(styles))
     gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.parallel(scripts))
 }
